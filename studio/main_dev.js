9dcd2f308454884658834d36603f69d07d4e0d7e
Mohamed El-Refaie <mohgamal@hayasolutions.com>
2022-01-29 05:05:56 +0200

    Remove Hello Message

--- studio/main_dev.js
+++ studio/main_dev.js
@@ -1,27 +1,33 @@
-import Studio from 'jsreport-studio'
+import Studio from "jsreport-studio";
 
 // a custom property section added to the templates
 const Properties = (props) => (
-  <div className='properties-section'>
-    <div className='form-group'>
+  <div className="properties-section">
+    <div className="form-group">
       <label>custom content</label>
       <input
-        type='text'
-        placeholder='a custom string'
+        type="text"
+        placeholder="a custom string"
         value={props.entity.custom}
-        onChange={(v) => props.onChange({ custom: v.target.value, _id: props.entity._id })}
+        onChange={(v) =>
+          props.onChange({ custom: v.target.value, _id: props.entity._id })
+        }
       />
     </div>
   </div>
-)
+);
 
-Studio.addPropertiesComponent('custom', Properties, (entity) => entity.__entitySet === 'templates')
+Studio.addPropertiesComponent(
+  "custom",
+  Properties,
+  (entity) => entity.__entitySet === "templates"
+);
 
 Studio.initializeListeners.push(async () => {
-  console.log('Doing some async initialization')
-})
+  console.log("Doing some async initialization");
+});
 
 Studio.readyListeners.push(async () => {
-  console.log('Studio is now rendered')
-  Studio.openModal(() => <span>Hello from the custom extension</span>)
-})
+  console.log("Studio is now rendered");
+  // Studio.openModal(() => <span>Hello from the custom extension</span>)
+});
